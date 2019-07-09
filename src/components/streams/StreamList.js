17707bf6a4938f20639c9b1525dcdd1750cfda80
Vincent Pace <vpace@nhpace.net>
2019-07-08 23:57:33 -0500

    Add modal for stream deletion

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -11,9 +11,9 @@ class StreamList extends React.Component {
     renderAdmin(stream) {
         if (stream.userId === this.props.currentUserId) {
             return (
-                <div>
+                <div className='right floated content'>
                     <Link to={`/streams/edit/${stream.id}`} className='ui button primary'>Edit</Link>
-                    <button className='ui button negative'>Delete</button>
+                    <Link to={`/streams/delete/${stream.id}`} className='ui button negative'>Delete</Link>
                 </div>
             );
         }
@@ -26,7 +26,7 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className='large middle aligned icon camera' />
                     <div className='content'>
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`}>{stream.title}</Link>
                         <div className='description'>{stream.description}</div>
                     </div>
                 </div>

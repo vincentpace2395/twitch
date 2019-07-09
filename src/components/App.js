d10432a9c17cbda87d4bd704cd3c476971fcb79e
Vincent Pace <vpace@nhpace.net>
2019-07-09 18:11:56 -0500

    Implement StreamShow component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
 import history from '../history';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
@@ -14,12 +14,14 @@ const App = () => {
             <Router history={history}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>
+                    </div>
             </Router>
         </div>
     );

7230d7745ea1da3f7fbe0c65b083ea1c2cebe0d7
Vincent Pace <vpace@nhpace.net>
2019-07-08 19:43:39 -0500

    Implement StreamCreate, StreamList components

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+import {
+    CREATE_STREAM,
+    FETCH_STREAM,
+    FETCH_STREAMS,
+    EDIT_STREAM,
+    DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+    switch(action.type) {
+        case 'CREATE_STREAM':
+            return {
+                ...state, [action.payload.id]: action.payload
+            };
+        case 'FETCH_STREAM':
+            return {
+                ...state, [action.payload.id]: action.payload
+            };
+        case 'FETCH_STREAMS':
+            return {
+                ...state, ..._.map(action.payload, 'id')
+            };
+        case 'EDIT_STREAM':
+            return {
+                ...state, [action.payload.id]: action.payload
+            };
+        case 'DELETE_STREAM':
+            return _.omit(state, action.payload);
+
+
+        default:
+            return state;
+    }
+};
